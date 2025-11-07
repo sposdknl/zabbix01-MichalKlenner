60bddb52c1a46d03404286d561817c173448f5e0
Klenner Michal <klenner.michal@student.sposdk.cz>
2025-11-07 09:30:47 +0100

    Added screenshot and updated project files

--- Ubuntu/install-zabbix-agent2.sh
+++ Ubuntu/install-zabbix-agent2.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-# Instalace balicku net-tools
+
 sudo apt-get install -y net-tools
 
-# Stažení balíčku pro instalaci zabbix repo
-sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest+ubuntu22.04_all.deb
+ wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.0+ubuntu24.04_all.deb
+ dpkg -i zabbix-release_latest_7.0+ubuntu24.04_all.deb
+ apt update
+ 
+ apt install zabbix-agent2
 
-# Instalace meta balíčku
-sudo dpkg -i zabbix-release_latest+ubuntu22.04_all.deb
-
-# Aktualizace repository
-sudo apt-get update
-
-# Instalace meta balíčku
-sudo apt-get install -y zabbix-agent2 zabbix-agent2-plugin-*
-
-# Povoleni sluzby zabbix-agent2
-sudo systemctl enable zabbix-agent2
-
-# Restart sluzby zabbix-agent2
-sudo systemctl restart zabbix-agent2
-
+apt install zabbix-agent2-plugin-mongodb zabbix-agent2-plugin-mssql zabbix-agent2-plugin-postgresql
+systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2
 # EOF
